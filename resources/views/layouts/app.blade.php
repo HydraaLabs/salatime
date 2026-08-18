568ef17a1bb919e5aa531189568ca960bd57c6c7
darkizone <mint@linux.com>
2026-08-18 16:27:25 +0100

    Point favicon links to /assets/favicon/

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,12 +9,12 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="base-url" content="{{ url('/') }}">
     <meta name="author" content="{{ config('settings.application.company_name') }}">
-    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="icon" type="image/png" href="/assets/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/assets/favicon/favicon.svg" />
     <link rel="shortcut icon" href="/favicon.ico" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png" />
     <meta name="apple-mobile-web-app-title" content="SalaTime" />
-    <link rel="manifest" href="/site.webmanifest" />
+    <link rel="manifest" href="/assets/favicon/site.webmanifest" />
     <meta name="keywords" content="{{ config('settings.application.company_name') }}">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="shortcut icon" href="{{ asset(config('settings.application.web_icon')) }}" />
--- resources/views/layouts/auth/auth_master.blade.php
+++ resources/views/layouts/auth/auth_master.blade.php
@@ -4,12 +4,12 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="icon" type="image/png" href="/assets/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/assets/favicon/favicon.svg" />
     <link rel="shortcut icon" href="/favicon.ico" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png" />
     <meta name="apple-mobile-web-app-title" content="SalaTime" />
-    <link rel="manifest" href="/site.webmanifest" />
+    <link rel="manifest" href="/assets/favicon/site.webmanifest" />
     <meta property="og:title" content="{{config('settings.application.company_name')}}">
     <meta property="og:description" content="{{config('settings.application.company_name')}} Admin Panel">
     <meta property="og:image" content="{{config('settings.application.web_logo')}}">
